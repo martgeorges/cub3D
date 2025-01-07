0cd74c3ff058838313d8f01bb77256f9fb9530c0
Martin Georges <martingeorges@MacBook-Air-de-Martin.local>
2025-01-07 16:03:45 +0100

    added everything

---
+++ sources/main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgeorges <mgeorges@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/07 16:01:42 by mgeorges          #+#    #+#             */
+/*   Updated: 2025/01/07 16:03:31 by mgeorges         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+int main(int ac, char **av)
+{
+    //check args (nbr, si ils sont valides etc)
+    //ourvir la fenetre
+    // démarrer le programme
+    //
+}
